d208f526c6b3660c979eb426bebc1a01c9c1c7e6
Vibhuti0206 <56320137+Vibhuti0206@users.noreply.github.com>
2020-04-21 23:21:11 +0530

    Update Noble Integer.cpp

--- Noble Integer.cpp
+++ Noble Integer.cpp
@@ -1,15 +1,35 @@
 int Solution::solve(vector<int> &A) {
-   int n=A.size();
-    sort(A.begin(),A.end());
-   for(int i=0;i<n-1;i++){
-       if(A[i]==A[i+1]) 
-         continue;
-       if(A[i]==n-i-1)
-        return 1;
-       
-   }
-   if(A[n-1] == 0)  
-    return 1;
-   return -1;
+	sort(A.begin(), A.end()) ;
+	int size = A.size();
+	for(int i=0;i<size;i++){
+		while(i+1<size && A[i]==A[i+1])
+			i++;
+		if(A[i]==size-1-i)
+			return 1;
+	}
+	return -1;
+}
+
+OR
+//the alternative is not the optimal solution
+
+int Solution::solve(vector<int> &A) {
+    int n = A.size();
+    
    
+    sort(A.begin(),A.end());
+    for(int i=0;i<n;i++)
+    {
+        int count  = 0 ;
+        for(int j=0;j<n;j++)
+        {
+            if(A[j] > A[i])
+             count++;
+            if(count > A[i])
+              break;
+        }
+        if(count == A[i])
+        return 1;
+    }
+    return -1;
 }
